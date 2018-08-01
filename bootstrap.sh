5ca892e31dd98384f036662840253f223fbd4cc6
Martynas <packmaster@riawolf.com>
2018-08-01 12:49:39 +0300

    latest files

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
 echo "-- Install tools and helpers --"
-sudo apt-get install -y --force-yes python-software-properties vim htop curl git npm
+sudo apt-get install -y --force-yes python-software-properties vim htop curl
 
 echo "-- Install PPA's --"
 sudo add-apt-repository ppa:ondrej/php
@@ -30,34 +30,12 @@ sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/ph
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
 sudo a2enmod rewrite
 
-echo "-- Creating virtual hosts --"
-sudo ln -fs /vagrant/public/ /var/www/app
-cat << EOF | sudo tee -a /etc/apache2/sites-available/default.conf
-<Directory "/var/www/">
-    AllowOverride All
-</Directory>
-
-<VirtualHost *:80>
-    DocumentRoot /var/www/app
-    ServerName app.local
-</VirtualHost>
-
-<VirtualHost *:80>
-    DocumentRoot /var/www/phpmyadmin
-    ServerName phpmyadmin.local
-</VirtualHost>
-EOF
-sudo a2ensite default.conf
-
 echo "-- Restart Apache --"
 sudo /etc/init.d/apache2 restart
 
 echo "-- Install phpMyAdmin --"
 wget -k https://files.phpmyadmin.net/phpMyAdmin/4.0.10.11/phpMyAdmin-4.0.10.11-english.tar.gz
 sudo tar -xzvf phpMyAdmin-4.0.10.11-english.tar.gz -C /var/www/
+suro rm -r /var/www/html
 sudo rm phpMyAdmin-4.0.10.11-english.tar.gz
-sudo mv /var/www/phpMyAdmin-4.0.10.11-english/ /var/www/phpmyadmin
-
-echo "-- Setup databases --"
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -uroot -proot -e "CREATE DATABASE my_database";
+sudo mv /var/www/phpMyAdmin-4.0.10.11-english/ /var/www/html
