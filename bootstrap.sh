f6e6992fd38a886ec171b4c628bb47fbf0fbcb7b
Martynas <packmaster@riawolf.com>
2018-08-01 12:24:25 +0300

    latest files

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,11 +19,9 @@ sudo add-apt-repository ppa:ondrej/php
 sudo add-apt-repository ppa:chris-lea/redis-server
 Update
 
-echo "-- Install NodeJS --"
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 
 echo "-- Install packages --"
-sudo apt-get install -y --force-yes apache2 mysql-server git-core nodejs rabbitmq-server redis-server
+sudo apt-get install -y --force-yes apache2 mysql-server git-core
 sudo apt-get install -y --force-yes php7.0-common php7.0-dev php7.0-json php7.0-opcache php7.0-cli libapache2-mod-php7.0 php7.0 php7.0-mysql php7.0-fpm php7.0-curl php7.0-gd php7.0-mcrypt php7.0-mbstring php7.0-bcmath php7.0-zip
 Update
 
@@ -53,11 +51,6 @@ sudo a2ensite default.conf
 
 echo "-- Restart Apache --"
 sudo /etc/init.d/apache2 restart
-
-echo "-- Install Composer --"
-curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
 
 echo "-- Install phpMyAdmin --"
 wget -k https://files.phpmyadmin.net/phpMyAdmin/4.0.10.11/phpMyAdmin-4.0.10.11-english.tar.gz
